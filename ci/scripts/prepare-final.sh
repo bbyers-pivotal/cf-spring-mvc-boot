ec29895234035a818a2b565bc8c34a228debe4c4
Brian Byers <brian@xclo.io>
2017-10-13 12:49:10 -0500

    check directory

--- ci/scripts/prepare-final.sh
+++ ci/scripts/prepare-final.sh
@@ -4,4 +4,6 @@ set -e
 version=`cat version/number`
 
 echo "Renaming release-candidate to final build"
+ls release-candidate
+ls final-dir
 cp release-candidate/${base_name}*.jar final-dir/${base_name}-${version}.jar
